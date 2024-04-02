19a5a7b6ecd0d02d12b73c5876001795013e76b0
frankknoll <Knoll_Frank@web.de>
2024-04-02 22:23:53 +0800

    refactoring

--- Java/src/main/java/de/keineInsektenImEssen/textdetector/service/MatcherFactory.java
+++ Java/src/main/java/de/keineInsektenImEssen/textdetector/service/MatcherFactory.java
@@ -4,20 +4,20 @@ import java.util.function.Predicate;
 
 public class MatcherFactory {
 
-    public static Predicate<String> contains(final String str) {
-        return elementText -> elementText.contains(str);
+    public static Predicate<String> contains(final String needle) {
+        return haystack -> haystack.contains(needle);
     }
 
-    public static Predicate<String> containsIgnoreCase(final String str) {
-        return elementText -> elementText.toUpperCase().contains(str.toUpperCase());
+    public static Predicate<String> containsIgnoreCase(final String needle) {
+        return haystack -> haystack.toUpperCase().contains(needle.toUpperCase());
     }
 
     public static Predicate<String> startsWith(final String prefix) {
-        return elementText -> elementText.startsWith(prefix);
+        return haystack -> haystack.startsWith(prefix);
     }
 
     public static Predicate<String> endsWith(final String suffix) {
-        return elementText -> elementText.endsWith(suffix);
+        return haystack -> haystack.endsWith(suffix);
     }
 
     public static Predicate<String> equalsIgnoreCase(final String str) {
