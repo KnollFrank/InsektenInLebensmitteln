1aaf57f8fa52c39a8d2c271c8b478370a5ec1055
Frank Knoll <Knoll_Frank@web.de>
2024-04-17 09:59:16 +0200

    refactoring

--- Java/src/main/java/de/keineInsektenImEssen/textdetector/service/UnwantedIngredientsProvider.java
+++ Java/src/main/java/de/keineInsektenImEssen/textdetector/service/UnwantedIngredientsProvider.java
@@ -4,7 +4,7 @@ import com.google.common.collect.ImmutableSet;
 import de.keineInsektenImEssen.textdetector.model.Pattern;
 import de.keineInsektenImEssen.textdetector.model.Synonym;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 import static de.keineInsektenImEssen.textdetector.service.MatcherFactory.contains;
@@ -19,37 +19,37 @@ public class UnwantedIngredientsProvider {
             ImmutableSet.of(
                     new Synonym(
                             "E120",
-                            Arrays.asList(
+                            List.of(
                                     new Pattern("E 120", contains("E 120")),
                                     new Pattern("E120", contains("E120")),
                                     new Pattern("Schildlaus", containsIgnoreCase("Schildlaus")),
                                     new Pattern("Karmin", containsIgnoreCase("Karmin")))),
                     new Synonym(
                             "E904",
-                            Arrays.asList(
+                            List.of(
                                     new Pattern("E 904", contains("E 904")),
                                     new Pattern("E904", contains("E904")),
                                     new Pattern("Schellack", containsIgnoreCase("Schellack")))),
                     new Synonym(
                             "Tenebrio molitor",
-                            Arrays.asList(
+                            List.of(
                                     new Pattern("Tenebrio molitor", containsIgnoreCase("Tenebrio molitor")),
                                     new Pattern("Mehlkäfer", containsIgnoreCase("Mehlkäfer")),
                                     new Pattern("Mehlwurm", containsIgnoreCase("Mehlwurm")),
                                     new Pattern("Mehlwürmer", containsIgnoreCase("Mehlwürmer")))),
                     new Synonym(
                             "Locusta migratoria",
-                            Arrays.asList(
+                            List.of(
                                     new Pattern("Locusta migratoria", containsIgnoreCase("Locusta migratoria")),
                                     new Pattern("Wanderheuschrecke", containsIgnoreCase("Wanderheuschrecke")))),
                     new Synonym(
                             "Acheta domesticus",
-                            Arrays.asList(
+                            List.of(
                                     new Pattern("Acheta domesticus", containsIgnoreCase("Acheta domesticus")),
                                     new Pattern("Hausgrille", containsIgnoreCase("Hausgrille")))),
                     new Synonym(
                             "Alphitobius diaperinus",
-                            Arrays.asList(
+                            List.of(
                                     new Pattern("Alphitobius diaperinus", containsIgnoreCase("Alphitobius diaperinus")),
                                     new Pattern("Buffalowurm", containsIgnoreCase("Buffalowurm")),
                                     new Pattern("Buffalowürmer", containsIgnoreCase("Buffalowürmer")),
