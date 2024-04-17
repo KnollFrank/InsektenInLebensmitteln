79201f434b9bbcc23555647db857f3cd877c90b3
Frank Knoll <Knoll_Frank@web.de>
2024-04-17 23:46:46 +0200

    Wenn bei der Suche nichts gefunden wird, dann wie in der App den Text "nichts gefunden" oder so einblenden.

--- docs/SearchController.js
+++ docs/SearchController.js
@@ -1,26 +1,38 @@
 class SearchController {
 
     #searchUI;
+    #productSearcher;
     #productsAndCategoriesView;
 
     constructor(searchUI, productSearcher, productsAndCategoriesView) {
         this.#searchUI = searchUI;
+        this.#productSearcher = productSearcher;
         this.#productsAndCategoriesView = productsAndCategoriesView;
         searchUI.form.addEventListener(
             'submit',
             e => {
                 e.preventDefault();
-                productsAndCategoriesView.displayProductsAndCategories(
-                    {
-                        products: productSearcher.searchProductsBy(searchUI.input.value),
-                        categories: []
-                    });
+                this.#search(searchUI.input.value);
             });
     }
 
     reset() {
         this.#resetAndFocus(this.#searchUI.input);
         this.#productsAndCategoriesView.displayProductsAndCategories({ products: [], categories: [] });
+        UIUtils.setVisible(this.#searchUI.noSearchResults, false);
+    }
+
+    #search(search) {
+        this.#showProducts(this.#productSearcher.searchProductsBy(search));
+    }
+
+    #showProducts(products) {
+        UIUtils.setVisible(this.#searchUI.noSearchResults, products.length === 0);
+        this.#productsAndCategoriesView.displayProductsAndCategories(
+            {
+                products: products,
+                categories: []
+            });
     }
 
     #resetAndFocus(input) {
