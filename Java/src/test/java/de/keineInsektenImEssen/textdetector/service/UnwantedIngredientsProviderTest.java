b70700fb72b9b2ac1a5c1888fef4131237f71d28
Frank Knoll <Knoll_Frank@web.de>
2024-04-19 09:05:15 +0200

    refining UnwantedIngredientsProviderTest

--- Java/src/test/java/de/keineInsektenImEssen/textdetector/service/UnwantedIngredientsProviderTest.java
+++ Java/src/test/java/de/keineInsektenImEssen/textdetector/service/UnwantedIngredientsProviderTest.java
@@ -2,34 +2,42 @@ package de.keineInsektenImEssen.textdetector.service;
 
 import de.keineInsektenImEssen.textdetector.model.Synonym;
 import de.keineInsektenImEssen.textdetector.model.Synonyms;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class UnwantedIngredientsProviderTest {
 
-    @Test
-    public void shouldFindAllMatches() {
+    @ParameterizedTest
+    @MethodSource("haystackAndMatchesProvider")
+    public void shouldFindAllMatches(final String haystack, final Set<String> matchesExpected) {
         // Given
         final Set<Synonym> unwantedIngredients = UnwantedIngredientsProvider.unwantedIngredients;
 
         // When
-        final Set<String> matches =
-                Synonyms.findAllMatches(
-                        unwantedIngredients,
+        final Set<String> matches = Synonyms.findAllMatches(unwantedIngredients, haystack);
+
+        // Then
+        assertThat(matches, is(matchesExpected));
+    }
+
+    private static Stream<Arguments> haystackAndMatchesProvider() {
+        return Stream.of(
+                arguments(
                         """
                                 E120, Schildlaus, Karmin, E904, Schellack, Tenebrio molitor,
                                 Mehlkäfer, Mehlwurm, Locusta migratoria, Wanderheuschrecke, Acheta domesticus,
-                                Hausgrille, Alphitobius diaperinus, Buffalowurm, Getreideschimmelkäfer""");
-
-        // Then
-        final Set<String> matchesExpected =
-                Set.of("E120", "Schildlaus", "Karmin", "E904", "Schellack", "Tenebrio molitor",
-                        "Mehlkäfer", "Mehlwurm", "Locusta migratoria", "Wanderheuschrecke", "Acheta domesticus",
-                        "Hausgrille", "Alphitobius diaperinus", "Buffalowurm", "Getreideschimmelkäfer");
-        assertThat(matches, is(matchesExpected));
+                                Hausgrille, Alphitobius diaperinus, Buffalowurm, Getreideschimmelkäfer""",
+                        Set.of("E120", "Schildlaus", "Karmin", "E904", "Schellack", "Tenebrio molitor",
+                                "Mehlkäfer", "Mehlwurm", "Locusta migratoria", "Wanderheuschrecke", "Acheta domesticus",
+                                "Hausgrille", "Alphitobius diaperinus", "Buffalowurm", "Getreideschimmelkäfer")),
+                arguments("E 120, E 904", Set.of("E 120", "E 904")));
     }
 }
